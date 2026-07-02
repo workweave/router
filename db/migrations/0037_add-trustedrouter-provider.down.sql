10b20bfb17110a74d5646cce1d776d175ce5a774
Joseph Perla <gh@jperla.com>
2026-07-02 07:33:50 -0700

    Address TrustedRouter provider exclusion review

--- db/migrations/0037_add-trustedrouter-provider.down.sql
+++ db/migrations/0037_add-trustedrouter-provider.down.sql
@@ -1,5 +1,8 @@
 BEGIN;
 
+DELETE FROM router.model_router_external_api_keys
+WHERE provider = 'trustedrouter';
+
 ALTER TABLE router.model_router_external_api_keys
   DROP CONSTRAINT model_router_external_api_keys_provider_check,
   ADD CONSTRAINT model_router_external_api_keys_provider_check
